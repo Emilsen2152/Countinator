7f10a8219a5f7a98c9bd0ac208e8403256d0f609
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-02-26 20:27:21 +0100

    Refactor guild deletion logic by moving it from ready.js to leftServer.js

--- src/events/guildDelete/leftServer.js
+++ src/events/guildDelete/leftServer.js
@@ -2,10 +2,6 @@ const blacklist = require("../../utils/countingBlacklist.js");
 const guilds = require("../../utils/guilds.js");
 
 module.exports = (guild, client) => {
-    console.log(`Left guild ${guild.name} (${
-        guild.id
-    })`);
-
     blacklist.deleteMany({
         guildId: guild.id
     }).exec();
