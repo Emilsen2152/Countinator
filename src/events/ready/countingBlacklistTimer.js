6cbaee81705a9e2b127f51471cbb0040658c5861
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-02-26 18:19:27 +0100

    Add counting blacklist expiration handler to notify users

---
+++ src/events/ready/countingBlacklistTimer.js
@@ -0,0 +1,21 @@
+const blacklist = require('../../utils/countingBlacklist.js');
+
+module.exports = async (client) => {
+    setInterval(async () => {
+        const blacklisted = await blacklist.find({
+            expiration: { $lt: Date.now() },
+            permanent: false
+        }).exec();
+
+        for (const blacklistedUser of blacklisted) {
+            if (blacklistedUser.expiration < Date.now() && !blacklistedUser.permanent) {
+                await blacklistedUser.deleteOne();
+                const user = await client.users.fetch(blacklistedUser.discordId);
+                const guild = client.guilds.cache.get(blacklistedUser.guildId);
+                user.send(`Your blacklist from the counting channel in ${guild.name} has expired.`).catch(e => {
+                    console.warn(e);
+                });
+            };
+        };
+    });
+};
