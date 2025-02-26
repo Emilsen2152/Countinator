25268804da68b0ad3e60c93eacbee64cbac7bc92
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-02-26 20:47:50 +0100

    Add counting event handlers for message updates and deletions

---
+++ src/events/messageDelete/counting.js
@@ -0,0 +1,36 @@
+const guilds = require('../../utils/guilds.js');
+const countingBlacklist = require('../../utils/countingBlacklist.js');
+
+module.exports = async (message) => {
+    if (message.author.bot) {
+        return;
+    };
+
+    if (!message.guild) {
+        return;
+    };
+
+    // Return if the user is blacklisted from counting
+    const blacklisted = await countingBlacklist.findOne({ discordId: message.author.id, guildId: message.guild.id }).exec();
+    
+    if (blacklisted) {
+        return;
+    };
+
+    const guild = await guilds.findById(message.guild.id).exec();
+
+    if (!guild) {
+        return;
+    }
+
+    if (message.channel.id !== guild.countingChannel) {
+        return;
+    };
+
+    message.channel.send(`<@${message.author.id}> Deleted their message!\nThe count has been restarted.\n**The next number is 1.**`);
+
+    guild.nextNumber = 1;
+    guild.lastNumberSenderId = '0';
+
+    await guild.save();
+};
---
+++ src/events/messageUpdate/counting.js
@@ -0,0 +1,36 @@
+const guilds = require('../../utils/guilds.js');
+const countingBlacklist = require('../../utils/countingBlacklist.js');
+
+module.exports = async (oldMessage, newMessage) => {
+    if (oldMessage.author.bot) {
+        return;
+    };
+
+    if (!oldMessage.guild) {
+        return;
+    };
+
+    // Return if the user is blacklisted from counting
+    const blacklisted = await countingBlacklist.findOne({ discordId: oldMessage.author.id, guildId: oldMessage.guild.id }).exec();
+    
+    if (blacklisted) {
+        return;
+    };
+
+    const guild = await guilds.findById(oldMessage.guild.id).exec();
+
+    if (!guild) {
+        return;
+    }
+
+    if (oldMessage.channel.id !== guild.countingChannel) {
+        return;
+    };
+
+    newMessage.channel.send(`<@${newMessage.author.id}> Edited their message!\nThe count has been restarted.\n**The next number is 1.**`);
+
+    guild.nextNumber = 1;
+    guild.lastNumberSenderId = '0';
+
+    await guild.save();
+};
