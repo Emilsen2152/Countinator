abc88cab30a1eac5cd397a83137ffb2ef3352b35
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-03-12 18:44:47 +0100

    Add ViewAuditLog permission for bot in setup command

--- src/commands/management/setup.js
+++ src/commands/management/setup.js
@@ -222,7 +222,7 @@ module.exports = {
     options: {
         devOnly: false,
         userPermissions: [],
-        botPermissions: [PermissionFlagsBits.AddReactions, PermissionFlagsBits.ManageMessages, PermissionFlagsBits.SendMessages, PermissionFlagsBits.ViewChannel],
+        botPermissions: [PermissionFlagsBits.AddReactions, PermissionFlagsBits.ManageMessages, PermissionFlagsBits.SendMessages, PermissionFlagsBits.ViewChannel, PermissionFlagsBits.ViewAuditLog],
         deleted: false,
     },
 };
