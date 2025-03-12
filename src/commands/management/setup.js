2dd01eb534cd75a988ed25d40aa1529e429cf113
Emilsen <79190987+Emilsen2152@users.noreply.github.com>
2025-03-12 10:07:15 +0100

    Add subcommands for server and competitive channel setup in management command

--- src/commands/management/setup.js
+++ src/commands/management/setup.js
@@ -26,7 +26,15 @@ module.exports = {
         .setName('setup')
         .setDescription('Setup the counting channel for your server. Use this command in the counting channel.')
         .setContexts(['Guild'])
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('server')
+                .setDescription('Setup the counting channel for your server. Use this command in the counting channel.'))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('competetive')
+                .setDescription('Setup the competetive counting channel for your server. Use this command in the competetive counting channel.')),
 
     run: async ({ interaction, client }) => {
         await interaction.deferReply().catch(console.warn);
@@ -44,7 +52,7 @@ module.exports = {
 
                     // Disable other buttons
                     i.message.components[0].components.forEach(button => {
-                       
+
                         button.data.disabled = true;
 
 
@@ -62,86 +70,131 @@ module.exports = {
 
         let guild = await guilds.findOne({ guildId: interaction.guild.id }).exec();
 
-        if (!guild) {
-            guild = new guilds({
-                guildId: interaction.guild.id,
-                countingChannel: interaction.channelId,
-            });
-        } else {
-            guild.countingChannel = interaction.channelId;
-        };
+        const subcommand = interaction.options.getSubcommand();
 
-        try {
-            interaction.editReply('Setup started.').catch(console.warn);
+        if (subcommand === 'server') {
+            if (!guild) {
+                guild = new guilds({
+                    guildId: interaction.guild.id,
+                    countingChannel: interaction.channelId,
+                });
+            } else {
+                guild.countingChannel = interaction.channelId;
+            };
 
-            const incorrectNumberRow = new ActionRowBuilder()
-                .addComponents(ignoreButton, deleteButton, restartButton);  
+            try {
+                interaction.editReply('Setup started.').catch(console.warn);
+
+                const incorrectNumberRow = new ActionRowBuilder()
+                    .addComponents(ignoreButton, deleteButton, restartButton);
+
+                let lastMessage = await interaction.channel.send({
+                    content: 'What should happen if the incorrect number is sent?',
+                    components: [incorrectNumberRow],
+                    withResponse: true
+                });
+
+                const incorrectNumberResponse = await collectResponse();
+
+                guild.incorrectNumber = incorrectNumberResponse.customId;
+                incorrectNumberResponse.reply('Registered.').catch(console.warn);
+
+                const wasLastSenderRow = new ActionRowBuilder()
+                    .addComponents(acceptButton, ignoreButton, deleteButton, restartButton);
+
+                lastMessage = await lastMessage.reply({
+                    content: 'What should happen if someone counts twice?',
+                    components: [wasLastSenderRow],
+                    withResponse: true,
+                });
+
+                const wasLastSenderResponse = await collectResponse();
+
+                guild.wasLastSender = wasLastSenderResponse.customId;
+                wasLastSenderResponse.reply('Registered.').catch(console.warn);
+
+                const notMathRow = new ActionRowBuilder()
+                    .addComponents(ignoreButton, deleteButton, restartButton);
+
+                lastMessage = await lastMessage.reply({
+                    content: 'What should happen if the message sent is not a mathematical expression?',
+                    components: [notMathRow],
+                    withResponse: true,
+                });
+
+                const notMathResponse = await collectResponse();
+
+                guild.notMath = notMathResponse.customId;
+                notMathResponse.reply('Registered.').catch(console.warn);
+
+                const notWholeNumberRow = new ActionRowBuilder()
+                    .addComponents(ignoreButton, deleteButton, restartButton);
+
+                lastMessage = await lastMessage.reply({
+                    content: 'What should happen if the message sent is not a whole number?',
+                    components: [notWholeNumberRow],
+                    withResponse: true,
+                });
+
+                const notWholeNumberResponse = await collectResponse();
+
+                guild.notWholeNumber = notWholeNumberResponse.customId;
+                notWholeNumberResponse.reply('Registered.').catch(console.warn);
+
+                await guild.save();
+
+                lastMessage.reply('Setup completed.').catch(console.warn);
+
+            } catch (error) {
+                console.warn(error);
+
+                if (error === 'No response was collected.') {
+                    await interaction.followUp('No response was collected. Please try again.').catch(console.warn);
+                } else {
+                    await interaction.followUp('An error occurred. Please try again.').catch(console.warn);
+                }
+            }
+        } else if (subcommand === 'competetive') {
+            if (!guild) {
+                guild = new guilds({
+                    guildId: interaction.guild.id,
+                    competetiveChannel: interaction.channelId,
+                });
+            } else {
+                guild.competetiveChannel = interaction.channelId;
+            };
+
+            const acceptTermsButton = new ButtonBuilder()
+                .setCustomId('AcceptTerms')
+                .setLabel('Accept')
+                .setStyle(ButtonStyle.Success);
+
+            const declineTermsButton = new ButtonBuilder()
+                .setCustomId('DeclineTerms')
+                .setLabel('Decline')
+                .setStyle(ButtonStyle.Danger);
             
+            const termsRow = new ActionRowBuilder()
+                .addComponents(acceptTermsButton, declineTermsButton);
+
             let lastMessage = await interaction.channel.send({
-                content: 'What should happen if the incorrect number is sent?',
-                components: [incorrectNumberRow],
+                content: 'By setting up the competetive counting channel you agree that the bot can show your server name in the global leaderboard.',
+                components: [termsRow],
                 withResponse: true
             });
 
-            const incorrectNumberResponse = await collectResponse();
+            const termsResponse = await collectResponse();
 
-            guild.incorrectNumber = incorrectNumberResponse.customId;
-            incorrectNumberResponse.reply('Registered.').catch(console.warn);
+            if (termsResponse.customId === 'DeclineTerms') {
+                return termsResponse.reply('Setup cancelled.').catch(console.warn);
+            };
 
-            const wasLastSenderRow = new ActionRowBuilder()
-                .addComponents(acceptButton, ignoreButton, deleteButton, restartButton);
-
-            lastMessage = await lastMessage.reply({
-                content: 'What should happen if someone counts twice?',
-                components: [wasLastSenderRow],
-                withResponse: true,
-            });
-
-            const wasLastSenderResponse = await collectResponse();
-
-            guild.wasLastSender = wasLastSenderResponse.customId;
-            wasLastSenderResponse.reply('Registered.').catch(console.warn);
-
-            const notMathRow = new ActionRowBuilder()
-                .addComponents(ignoreButton, deleteButton, restartButton);
-
-            lastMessage = await lastMessage.reply({
-                content: 'What should happen if the message sent is not a mathematical expression?',
-                components: [notMathRow],
-                withResponse: true,
-            });
-
-            const notMathResponse = await collectResponse();
-
-            guild.notMath = notMathResponse.customId;
-            notMathResponse.reply('Registered.').catch(console.warn);
-
-            const notWholeNumberRow = new ActionRowBuilder()
-                .addComponents(ignoreButton, deleteButton, restartButton);
-
-            lastMessage = await lastMessage.reply({
-                content: 'What should happen if the message sent is not a whole number?',
-                components: [notWholeNumberRow],
-                withResponse: true,
-            });
-
-            const notWholeNumberResponse = await collectResponse();
-
-            guild.notWholeNumber = notWholeNumberResponse.customId;
-            notWholeNumberResponse.reply('Registered.').catch(console.warn);
+            guild.nextCompetetiveNumber = 1;
+            guild.lastCompetetiveSender = '0';
 
             await guild.save();
 
             lastMessage.reply('Setup completed.').catch(console.warn);
-
-        } catch (error) {
-            console.warn(error);
-
-            if (error === 'No response was collected.') {
-                await interaction.followUp('No response was collected. Please try again.').catch(console.warn);
-            } else {    
-                await interaction.followUp('An error occurred. Please try again.').catch(console.warn);
-            }
         }
     },
 
