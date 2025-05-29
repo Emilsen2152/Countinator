bab23cb4631e5daf94a48f0ff42db9368610bfc7
Emil Velken Soldal <emil.v.soldal@gmail.com>
2025-05-29 22:58:39 +0200

    Add 'get-channels' command to retrieve channels for a specific guild

---
+++ src/commands/bot-mod-tools/get-channels.js
@@ -0,0 +1,33 @@
+const guilds = require('../../utils/guilds');
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('get-channels')
+        .setDescription('Get the channels for a specific guild.')
+        .setContexts(['Guild'])
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .addStringOption(option =>
+            option
+                .setName('guildId')
+                .setDescription('The ID of the guild to get the channels for.')
+                .setRequired(true)
+        ),            
+
+    run: async ({ interaction, client }) => {
+        await interaction.deferReply().catch(console.warn);
+
+        interaction.editReply({
+            content: 'Worked',
+            ephemeral: true,
+        }).catch(console.warn);
+    },
+
+    options: {
+        guildOnly: true,
+        devOnly: true,
+        userPermissions: [],
+        botPermissions: ['AddReactions', 'ManageMessages', 'SendMessages', 'ViewChannel', 'ViewAuditLog'],
+        deleted: false,
+    },
+};
